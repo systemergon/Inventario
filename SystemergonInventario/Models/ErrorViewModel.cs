b3c551514ab9fffde8427b69922ee40a56a21011
Jorge Rodriguez <systemergon@gmail.com>
2023-10-25 12:34:02 -0600

    Crear Proyecto Inventario

---
+++ SystemergonInventario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SystemergonInventario.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
